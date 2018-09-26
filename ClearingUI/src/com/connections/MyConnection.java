fa3f3c0126f3d40a6fc02e3cb9ea79145a69c7c4
ubuntu <ubuntu@ubuntu-HP-Pavilion-15-Notebook-PC>
2018-09-26 10:43:20 +0530

    password change

--- ClearingUI/src/com/connections/MyConnection.java
+++ ClearingUI/src/com/connections/MyConnection.java
@@ -10,7 +10,7 @@ public class MyConnection {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			//System.out.println("Driver Loaded Successfully");
 			//con = DriverManager.getConnection("jdbc:sqlserver://sql5007.site4now.net;databaseName=DB_A40FBC_cns065918", "DB_A40FBC_cns065918_admin","Admin_1234");
-			con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ClearingSettlement", "sa","Mssql@81326");
+			con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ClearingSettlement", "sa","sa123");
 			//System.out.println("Connection Done Successfully");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
